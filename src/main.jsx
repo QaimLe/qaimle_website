cbb10567c34b5aa8f4975021e6af78c4837d794c
khalid <khaledesam384@gmail.com>
2025-07-26 21:05:22 +0300

    feat: bootstrap dependencies added and auth0 integrated for login logout

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
+import authConfig from './authConfig';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Auth0Provider {...authConfig}>
     <App />
+    </Auth0Provider>
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+
+export default defineConfig(({mode}) => {
+    // Load app-level env vars to node-level env vars.
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return {
+    plugins: [react()],
+    base: '/',
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_URL || 'http://localhost:3000',
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          secure: process.env.NODE_ENV === 'production', // Only secure in production
+        },
+      },
+    },
+  };
+});
